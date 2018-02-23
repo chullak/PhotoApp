3712091d0ec24e558c61773d2ebb72f8d1894d18
Rahul Jain <chullak@gmail.com>
2018-02-22 23:44:33 -0500

    Adding try block so database will not be locked for forever

--- src/main/java/com/code/challenge/service/ApplicationService.java
+++ src/main/java/com/code/challenge/service/ApplicationService.java
@@ -14,30 +14,32 @@ import com.code.challenge.http.client.RestClient;
 @Component
 public class ApplicationService {
 	@Inject
-	DataLoadConfig  dataLoadConfig;
-	
+	DataLoadConfig dataLoadConfig;
+
 	@Inject
 	DBLockDao dbLockDao;
 
 	public boolean refresh() {
 		// add the entry in db Lock table to indicate the data loading in
-		// progress to keep distributed structure in mind 
-		if(!dbLockDao.isDataloadInProgress()){
-		dbLockDao.add();
-		Dao dao;
-		RestClient restClient;
-		for(Entry<RestClient, Dao> entry: dataLoadConfig.getDataloaders().entrySet())
-		{
-			restClient=entry.getKey();
-			dao=entry.getValue();
-			dao.deleteAll();
-			dao.saveAll(restClient.getList());
-			
-		}	
-		System.out.println("done");
-		dbLockDao.clear();
-		}else{
-			
+		// progress to keep distributed structure in mind
+		if (!dbLockDao.isDataloadInProgress()) {
+			dbLockDao.add();
+			try {
+				Dao dao;
+				RestClient restClient;
+				for (Entry<RestClient, Dao> entry : dataLoadConfig.getDataloaders().entrySet()) {
+					restClient = entry.getKey();
+					dao = entry.getValue();
+					dao.deleteAll();
+					dao.saveAll(restClient.getList());
+
+				}
+			} finally {
+				System.out.println("done");
+				dbLockDao.clear();
+			}
+		} else {
+
 			throw new RuntimeException("DataLoad is aready in progress");
 		}
 		return true;
